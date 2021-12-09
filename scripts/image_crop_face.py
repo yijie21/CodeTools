b0b5647c004dfa9a353e2416bef3d7a81ee439f4
1396599708@qq.com <FightingCF@1396599708@qq.com>
2021-12-09 14:00:22 +0800

    enter

--- scripts/image_crop_face.py
+++ scripts/image_crop_face.py
@@ -4,42 +4,43 @@ import os
 import glob
 import threading
 #最后剪裁的图片大小
-size_m = 64
-size_n = 64
 def detect(img, cascade):
-    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),flags=cv2.CASCADE_SCALE_IMAGE)
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(90, 90), maxSize=(128, 128), flags=cv2.CASCADE_SCALE_IMAGE)
     if len(rects) == 0:
         return []
     rects[:, 2:] += rects[:, :2]
     return rects
 
-cascade = cv2.CascadeClassifier("D:/Code/CodeTools/assets/haarcascade_frontalface_alt2.xml")
-imglist = glob.glob("D:/datasets/debug/*")
+cascade = cv2.CascadeClassifier("D:/Code/CodeTools/assets/haarcascade_frontalface_default.xml")
+imglist = glob.glob(os.path.join("D:/datasets/stars/", "*.jpg"))
 cnt = 0
 for list in imglist:
     img = cv2.imread(list)
     try:
-        if img.shape[0] < 64 or img.shape[1] < 64:
+        if img.shape[0] < 128 or img.shape[1] < 128:
             continue
+        dst=img
+        rects=detect(dst,cascade)
+        for x1,y1,x2,y2 in rects:
+            center = [(y1 + y2) // 2, (x1 + x2) // 2]
+            y_left = center[0] - 64
+            y_right = center[0] + 64
+            x_left = center[1] - 64
+            x_right = center[1] + 64
+            if y_left < 0:
+                y_right -= y_left
+                y_left = 0
+            if x_left < 0:
+                x_right -= x_left
+                x_left = 0
+            roi = dst[y_left: y_right, x_left: x_right]
+            f = "{}/{}".format("D:/datasets", "stars_crop")
+            if not os.path.exists(f):
+                os.mkdir(f)
+            cv2.imwrite("{}/{:0>6}.jpg".format(f, cnt), roi)
+            cnt += 1
     except:
         print(list)
-    dst=img
-    rects=detect(dst,cascade)
-    for x1,y1,x2,y2 in rects:
-        center = [(y1 + y2) // 2, (x1 + x2) // 2]
-        y_left = center[0] - 32
-        y_right = center[0] + 32
-        x_left = center[1] - 32
-        x_right = center[1] + 32
-        if y_left < 0:
-            y_right -= y_left
-            y_left = 0
-        if x_left < 0:
-            x_right -= x_left
-            x_left = 0
-        roi = dst[y_left: y_right, x_left: x_right]
-        f = "{}/{}".format("D:/datasets", "crop")
-        if not os.path.exists(f):
-            os.mkdir(f)
-        cv2.imwrite("{}/{}.jpg".format(f, cnt), roi)
-        cnt += 1
+        pass
+    continue
+    
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-import numpy as np
-print (np.linalg.norm([108, 108, 89]))
+import os
+img_folder = 'D:/datasets/stars/'
+img_paths = os.listdir(img_folder)
+for i, img_path in enumerate(img_paths):
+    os.rename(os.path.join(img_folder, img_path), 
+              os.path.join(img_folder, '{:0>6}.jpg'.format(i)))
