3333ed2b20bd6de359790a2cc93d81fcd9ea868c
1396599708@qq.com <FightingCF@1396599708@qq.com>
2022-01-09 10:50:09 +0800

    numpy extend

--- test.py
+++ test.py
@@ -4,3 +4,4 @@ img_paths = os.listdir(img_folder)
 for i, img_path in enumerate(img_paths):
     os.rename(os.path.join(img_folder, img_path), 
               os.path.join(img_folder, '{:0>6}.jpg'.format(i)))
+    
